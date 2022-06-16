2f9779673f8c03d68e7bede9f31414531afe2b02
lekan012704 <durosinmiabdulsobuur>
2022-06-16 10:37:04 +0100

    cool one

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
+from email.mime import image
+from turtle import title
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  image = models.ImageField(upload_to='images/')
+  author = models.CharField(max_length=100)
+  category = models.ForeignKey(category, on_delete=models.CASCADE)
+  slug = models.SlugField(max_length=100, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Blog")
+        verbose_name_plural = _("Blogs")
+
+    def __str__(self):
+        return self.name
+
+    def save:
+        slug = slugify(self.name)
+        self.slug = slugify(slug,allow_unicode=True
+        )
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+
+    def get_absolute_url(self):
+        return reverse("Blog_detail", kwargs={"pk": self.pk})
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def BlogName(request):
+    return render(request, template_name='index.html')
+
+def IndexPage(request):
+    return render(request, template_name='index.html')
